73841dd4e58cc8bd1497a53d50d1ee49eca3fb40
Artix1500 <artur.haczek23@gmail.com>
2019-06-18 09:27:23 +0200

    N3 alfa test version

--- index.js
+++ index.js
@@ -11,44 +11,55 @@ const store = new N3.Store();
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '4000mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '4000mb', extended: true}))
 
+function showMemUsage(){
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+
+    return Math.round(used * 100) / 100;
+}
 
 async function getObjs(sub, pred) {
-        const res = await store.getQuads(namedNode(sub), namedNode(pred), null);
-        const list = await res.map(data => data.object.value);
-        // console.log("getObjs")
-        // console.log(list);
-        if(list === []){
-            return null;
-        }
-        return list;
+    //showMemUsage()
+    const res = await store.getQuads(namedNode(sub), namedNode(pred), null);
+    return await res.map(data => data.object.value);
+    // const list = await res.map(data => data.object.value);
+    // // console.log("getObjs")
+    // // console.log(list);
+    // if(list === []){
+    //     return null;
+    // }
+    // return list;
 };
 
 async function getSingleStringValue(sub, pred) {
-        const res = await store.getQuads(namedNode(sub), namedNode(pred), null);
-        const list = await res.map(data => data.object.value);
-        // console.log("getSingleStringValue")
-        // console.log(list);
-        const quoted_val = list[0];
-        const val = quoted_val.replace('"', '').replace('"', '');
-        if(val === ''){
-            return null;
-        }
-        return val;
+    //showMemUsage()
+    const res = await store.getQuads(namedNode(sub), namedNode(pred), null);
+    // console.log("getSingleStringValue")
+    // console.log(list);
+    const quoted_val = res[0].object.value;
+    const val = quoted_val.replace('"', '').replace('"', '');
+    if(val === ''){
+        return null;
+    }
+    return val;
 };
 
 
 async function getSubs(type) {
+    //showMemUsage()
     const predicate = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
         const res = await store.getQuads(null, namedNode(predicate), namedNode(type));
-        const list = await res.map(data => data.subject.value);
-        // console.log("getSubs")
-        // console.log(list);
-        if(list === []){
-            return null;
-        }
-        return list;
+        return await res.map(data => data.subject.value);
+        // const list = await res.map(data => data.subject.value);
+        // // console.log("getSubs")
+        // // console.log(list);
+        // if(list === []){
+        //     return null;
+        // }
+        // return list;
 };
 
 
@@ -120,20 +131,30 @@ app.get('/', async (req,res,next) => {
 
 
 app.post('/api/upload',async (req, res) => {
+    var start = new Date().getTime();
+
     try {
+        console.log("Before:")
+        await showMemUsage();
         const todo = req.body;
         const rdf = await jsonld.toRDF(todo, {format: 'application/n-quads'});
-        console.log(rdf);
         const parser = new N3.Parser();
         const quads = parser.parse(rdf);
+        console.log(quads);
         await quads.forEach( quad => store.addQuad(quad))
-
+        console.log("After:")
+        await showMemUsage();
     } catch (error) {
         return res.status(500).send({
             success: 'false',
             message:  error
         })
     }
+
+    var end = new Date().getTime();
+    var time = end - start;
+    console.log('Execution time on local of data loading: ' + time/1000 + ' s.');
+
     return res.status(201).send({
         success: 'true',
         message: 'added successfully'
