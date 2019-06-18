4bb5b9a795718a63c718984c093f99e32b923ee3
Artix1500 <artur.haczek23@gmail.com>
2019-06-18 16:18:09 +0200

    Create basic resolver generator

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,19 +1,21 @@
 
-class rootResolver{
-	constructor(db) {
-		this.database = db;
-    
+class rootResolver {
+    constructor(db) {
+        this.database = db;
+        const json = require('./schema-mapping.json');
+
+
         this.rootResolver = {
             Query: {
-                Person_GET: () =>{
+                Person_GET: () => {
                     return this.database.getSubs("http://schema.org/Person");
                 },
-                Organization_GET: () =>{
+                Organization_GET: () => {
                     return this.database.getSubs("http://schema.org/Organization");
                 }
             },
             Organization: {
-                _id: (parent) => {return parent},
+                _id: (parent) => { return parent },
                 legalName: (parent) => { return this.database.getSingleStringValue(parent, "http://schema.org/legalName") },
                 employee: (parent) => { return this.database.getObjs(parent, "http://schema.org/employee") }
             }
@@ -21,7 +23,7 @@ class rootResolver{
 
         let newResolver = "Person"
         let newResolverBody = {}
-        newResolverBody['_id'] = (parent) => {return parent}
+        newResolverBody['_id'] = (parent) => { return parent }
         newResolverBody['name'] = (parent) => { return this.database.getSingleStringValue(parent, "http://schema.org/name") }
         newResolverBody['affiliation'] = (parent) => { return this.database.getObjs(parent, "http://schema.org/affiliation") }
 
@@ -29,7 +31,66 @@ class rootResolver{
         this.rootResolver[newResolver] = newResolverBody
 
 
-        
+        let objects = []
+
+        for (var property in json.types) {
+            if (json.types.hasOwnProperty(property)) {
+                objects.push(json.types[property]);
+            }
+        }
+
+        objects.forEach(element => {
+            console.log("\nNEW OBJECT\n")
+
+            if (element.type  !== 'ObjectType') {
+                // TYPE
+
+                let newResolver = element.name
+                let newResolverBody = {}
+
+                for (var property in element.fields) {
+                    if(element.fields[property]['name']  === '_value'){
+                        newResolverBody['_value'] = (parent) => { return parent }
+                    }
+                    newResolverBody[element.fields[property]['name']] = (parent) => { return this.database.getSingleStringValue(parent, "http://schema.org/"+element.fields[property]['name']) }
+                }
+
+                this.rootResolver[newResolver] = newResolverBody
+
+
+            } else {
+                // OBJECT
+
+                let newResolver = element.name
+                let newResolverBody = {}
+
+                for (var property in element.fields) {
+                    if(element.fields[property]['name']  === '_id'){
+                        newResolverBody['_id'] = (parent) => { return parent }
+                    }
+                    else if(element.fields[property]['name']  === '_type'){
+                        newResolverBody[element.fields[property]['name']] = (parent) => { return this.database.getObjs(parent, "http://www.w3.org/1999/02/22-rdf-syntax-ns#type") }
+                    }
+                    else{
+                        if(json.types[element.fields[property]['name']] !== undefined){
+                            newResolverBody[element.fields[property]['name']] = (parent) => { return this.database.getObjs(parent, "http://schema.org/"+element.fields[property]['name']) }
+                        }
+                        else{
+                            newResolverBody[element.fields[property]['name']] = (parent) => { return this.database.getSingleStringValue(parent, "http://schema.org/"+element.fields[property]['name']) }
+                        }
+                    }
+                }
+
+                this.rootResolver[newResolver] = newResolverBody
+
+            }
+
+        });
+
+        console.log("\n\n\n\n\n\n\n\n\n\n")
+        console.dir(this.rootResolver);
+
+
     }
 }
 
--- schema/schema.js
+++ schema/schema.js
@@ -1,18 +1,37 @@
 schemaString = `
-        type Query {
-            Person_GET: [Person]
-            Organization_GET: [Organization]
-        }
-        type Organization {
-            _id: ID!
-            legalName: String
-            employee: [Person]
-        }
-        type Person {
-            _id: ID!
-            name: String
-            affiliation: [Organization]
-        }
-        `
-        
+type Query {
+    Organization_GET: [Organization]
+    Person_GET: [Person]
+}
+
+type Organization {
+_id: ID!
+_type: [String]
+legalName: Text
+address: Text
+department: [Organization]
+employee: [Person]
+email: Text
+numberOfEmployees: Integer
+
+}
+
+type Person {
+_id: ID!
+_type: [String]
+name: Text
+affiliation: Organization
+}
+
+type Text {
+_type: [String]
+_value: String
+}
+
+type Integer {
+_type: [String]
+_value: String
+}
+`
+
 module.exports = schemaString
