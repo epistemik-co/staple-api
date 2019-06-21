c8eee45d44c373b42fb5253b79f0dfb4ff8a0863
Artix1500 <artur.haczek23@gmail.com>
2019-06-21 10:39:11 +0200

    Refactor for schemaTree

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,10 +1,11 @@
 const { buildSchemaFromTypeDefinitions } = require('graphql-tools');
 const schemaString = require('../schema/schema');
+const schemaTreeClass = require('./schemaTree')
+const createTree = require('./schemaTree')
 
 class rootResolver {
     constructor(db) {
         this.database = db;
-        const json = require('./schema-mapping.json');
 
         const schema = buildSchemaFromTypeDefinitions(schemaString);
 
@@ -47,94 +48,18 @@ class rootResolver {
 
         // this.rootResolver[newResolver] = newResolverBody
 
-        const systemTypes = [
-            'Query',
-            'ID',
-            'String',
-            '__Schema',
-            '__Type',
-            '__TypeKind',
-            'Boolean',
-            '__Field',
-            '__InputValue',
-            '__EnumValue',
-            '__Directive',
-            '__DirectiveLocation',
-            'Int',
-            'Float',
-            'Boolean',
-        ];
 
 
-        // chodzenie po drzewie
 
-        var treeFromSchema = {}
-        let objectsFromSchemaMapping = []
+        // -------------------------------------------------- RENDER SCHEMA + SCHEMA-MAPPING TREE
+        
+        const schemaTree = createTree();
 
-        for (var property in json.types) {
-            objectsFromSchemaMapping.push( json.types[property] );
-        }
+        
+        // console.dir(schemaTree);
+        // -------------------------------------------------- SAVE TO FILE
 
-
-        for(let schemaTypeName in schema.getTypeMap()){
-            // skip system types
-            if(systemTypes.indexOf(schemaTypeName) > -1){
-                continue;
-            }
-
-            let newNode = {};
-            newNode['name'] = schema.getTypeMap()[schemaTypeName]['name'];
-            let newNodeFromMappingTree = {};
-
-            objectsFromSchemaMapping.forEach(object => {
-                if(object['name'] === schema.getTypeMap()[schemaTypeName]['name']){
-                    newNode['uri'] = object['uri'];
-                    newNode['type'] = object['type'];
-                    newNodeFromMappingTree = object.fields;
-                }
-            });
-
-            let newNodeData = {};
-            schema.getTypeMap()[schemaTypeName].astNode.fields.map(object => {
-                newNodeData[object.name.value] = {};
-                let copyOfNewNode = newNodeData[object.name.value];
-                let prop = object['type'];
-
-                while(prop !== undefined){
-                    // save information if it is mandatory and skip
-                    if(prop['kind'] === 'NonNullType'){
-                        copyOfNewNode['mandatory'] = true;
-                        prop = prop['type'];
-                    }
-                    else{
-                        copyOfNewNode['mandatory'] = false;
-                    }
-                    // this is the root where we get the value
-                    if(prop['kind'] === 'NamedType'){
-                        // console.log(prop)
-                        copyOfNewNode['name'] = prop['name']['value']
-                        newNodeFromMappingTree.forEach(object2 => {
-                            if(object2['name'] === object.name.value){
-                                copyOfNewNode['uri'] = object2['uri'];
-                            }
-                        });
-                    }
-                    else{
-                        copyOfNewNode['kind'] = prop['kind']
-                        copyOfNewNode['data'] = {}
-                    }
-                    prop = prop['type'];
-                    copyOfNewNode = copyOfNewNode['data'];
-                }
-            });
-
-            newNode['data'] = newNodeData;
-            treeFromSchema[schema.getTypeMap()[schemaTypeName]['name']] = newNode;
-        }
-
-        // SAVE TO FILE
-
-        // var jsonContent = JSON.stringify(treeFromSchema)
+        // var jsonContent = JSON.stringify(schemaTree)
         // const fs = require('fs');
         // fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
         //     if (err) {
@@ -151,124 +76,73 @@ class rootResolver {
 
 
 
-        // for(let schemaTypeName in schema.getTypeMap()){
-        //     // skip system types
-        //     if(systemTypes.indexOf(schemaTypeName) > -1){
-        //         continue;
-        //     }
-        //     const schemaTypeObject = schema.getTypeMap()[schemaTypeName].astNode;
+        // -------------------------------------------------- CREATE RESOLVERS
 
-        //     // console.log("\nschemaTypeName = " + schemaTypeName + " schemaTypeObject['name']['value'] = " + schemaTypeObject['name']['value'] + " \n")
-
-        //     const arrayOfFieldsKind = schemaTypeObject.fields.map(x => x['type']['kind']);
-
-        //     const arrayOfFields = schemaTypeObject.fields.map(x => {
-        //        if( x['type']['kind'] === 'NamedType' ) { return x['type']['name']} else { return  x['type']['type'] }
-        //     });
-
-
-        //     // console.log(arrayOfFields.map(x => {
-        //     //     if( x['kind'] === 'Name' ) { return x['value']} else { return  "[" + x['name']['value'] + "]" }
-        //     //  }));
-
-        // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-        objectsFromSchemaMapping.forEach(async object => {
-            // console.log("\nNEW OBJECT\n")
-            // const schemaForObject = schema.getTypeMap()[object['name']];
-            // console.log(schemaForObject.astNode.fields.map(x => x['name']['value']))
+        // objectsFromSchemaMapping.forEach(async object => {
+        //     // console.log("\nNEW OBJECT\n")
+        //     // const schemaForObject = schema.getTypeMap()[object['name']];
+        //     // console.log(schemaForObject.astNode.fields.map(x => x['name']['value']))
             
-            if (object.type  !== 'ObjectType') {
-                // TYPE
-                // console.log(object)
+        //     if (object.type  !== 'ObjectType') {
+        //         // TYPE
+        //         // console.log(object)
 
-                let newResolver = object.name
-                let newResolverBody = {}
+        //         let newResolver = object.name
+        //         let newResolverBody = {}
 
-                for (var property in object.fields) {
-                    if(object.fields[property]['name']  === '_value'){
-                        newResolverBody['_value'] = async (parent) => {return parent.value}
-                    }
-                    else if(object.fields[property]['name']  === '_type'){
-                        newResolverBody['_type'] = (parent) => {return [parent.datatype.value] }
-                    }
-                }
+        //         for (var property in object.fields) {
+        //             if(object.fields[property]['name']  === '_value'){
+        //                 newResolverBody['_value'] = async (parent) => {return parent.value}
+        //             }
+        //             else if(object.fields[property]['name']  === '_type'){
+        //                 newResolverBody['_type'] = (parent) => {return [parent.datatype.value] }
+        //             }
+        //         }
 
-                this.rootResolver[newResolver] = newResolverBody
+        //         this.rootResolver[newResolver] = newResolverBody
                 
 
 
-            } else {
-                // OBJECT
-                // console.log(object)
+        //     } else {
+        //         // OBJECT
+        //         // console.log(object)
 
-                let newResolver = object.name 
-                let newResolverBody = {}
+        //         let newResolver = object.name 
+        //         let newResolverBody = {}
 
-                for (var property in object.fields) {
-                    if(object.fields[property]['name']  === '_id'){
-                        newResolverBody['_id'] = async (parent) => { return await parent }
-                    }
-                    else if(object.fields[property]['name']  === '_type'){
-                        newResolverBody[object.fields[property]['name']] = async (parent) => { return await this.database.getObjs(parent, "http://www.w3.org/1999/02/22-rdf-syntax-ns#type") }
-                    }
-                    else{
-                        // console.log("object.fields[property]")
-                        // console.log(object.fields[property]);
-                        // console.log()
-                        if(schema.getTypeMap()[object.fields[property]['name']] !== undefined){ // this is bad
+        //         for (var property in object.fields) {
+        //             if(object.fields[property]['name']  === '_id'){
+        //                 newResolverBody['_id'] = async (parent) => { return await parent }
+        //             }
+        //             else if(object.fields[property]['name']  === '_type'){
+        //                 newResolverBody[object.fields[property]['name']] = async (parent) => { return await this.database.getObjs(parent, "http://www.w3.org/1999/02/22-rdf-syntax-ns#type") }
+        //             }
+        //             else{
+        //                 // console.log("object.fields[property]")
+        //                 // console.log(object.fields[property]);
+        //                 // console.log()
+        //                 if(schema.getTypeMap()[object.fields[property]['name']] !== undefined){ // this is bad
                             
                             
-                            const name = "http://schema.org/"+object.fields[property]['name'];
-                            let constr = (name) => { return (parent) => { return this.database.getObjs(parent, name) } };
-                            newResolverBody[object.fields[property]['name']] =  constr(name);
-                        }
-                        else{
+        //                     const name = "http://schema.org/"+object.fields[property]['name'];
+        //                     let constr = (name) => { return (parent) => { return this.database.getObjs(parent, name) } };
+        //                     newResolverBody[object.fields[property]['name']] =  constr(name);
+        //                 }
+        //                 else{
                             
-                            const name = "http://schema.org/" + object.fields[property]['name'];
-                            let constr = (name) => { return  ((parent) => {return this.database.getSingleLiteral(parent, name) })};
-                            newResolverBody[object.fields[property]['name']] = constr(name);
-                        }
-                    }
+        //                     const name = "http://schema.org/" + object.fields[property]['name'];
+        //                     let constr = (name) => { return  ((parent) => {return this.database.getSingleLiteral(parent, name) })};
+        //                     newResolverBody[object.fields[property]['name']] = constr(name);
+        //                 }
+        //             }
                     
-                }
+        //         }
 
-                this.rootResolver[newResolver] = newResolverBody
+        //         this.rootResolver[newResolver] = newResolverBody
 
-            }
+        //     }
 
-        });
+        // });
 
         // console.log("\n\n")
         // console.dir(this.rootResolver);
---
+++ resolvers/schemaTree.js
@@ -0,0 +1,94 @@
+const { buildSchemaFromTypeDefinitions } = require('graphql-tools');
+const schemaString = require('../schema/schema');
+const schemaMapping = require('./schema-mapping');
+
+// -------------------------------------------------- RENDER SCHEMA + SCHEMA-MAPPING TREE
+createTree = () => {
+    const schema = buildSchemaFromTypeDefinitions(schemaString);
+    let treeFromSchema = {};
+
+    const systemTypes = [
+        'Query',
+        'ID',
+        'String',
+        '__Schema',
+        '__Type',
+        '__TypeKind',
+        'Boolean',
+        '__Field',
+        '__InputValue',
+        '__EnumValue',
+        '__Directive',
+        '__DirectiveLocation',
+        'Int',
+        'Float',
+        'Boolean',
+    ];
+
+    let objectsFromSchemaMapping = [];
+
+    for (var property in schemaMapping.types) { objectsFromSchemaMapping.push(schemaMapping.types[property]); };
+
+    for (let schemaTypeName in schema.getTypeMap()) {
+        let newNode = {}; // object that will be added to tree
+        let newNodeFromMappingTree = {}; // node that contain data from schema-mapping for specyfic object
+        let newNodeData = {}; // data that will be added as a field to newNode
+
+        // skip system types
+        if (systemTypes.indexOf(schemaTypeName) > -1) {
+            continue;
+        }
+
+        // create field for type field from schema
+        newNode['name'] = schema.getTypeMap()[schemaTypeName]['name'];
+        
+        // find uri and type of the type field from schema
+        objectsFromSchemaMapping.forEach(object => {
+            if (object['name'] === schema.getTypeMap()[schemaTypeName]['name']) {
+                newNode['uri'] = object['uri'];
+                newNode['type'] = object['type'];
+                newNodeFromMappingTree = object.fields;
+            }
+        });
+
+        schema.getTypeMap()[schemaTypeName].astNode.fields.map(object => {
+            newNodeData[object.name.value] = {};
+            let copyOfNewNode = newNodeData[object.name.value];
+            let prop = object['type'];
+
+            while(prop !== undefined){
+                // save information if it is mandatory and skip
+                if(prop['kind'] === 'NonNullType'){
+                    copyOfNewNode['mandatory'] = true;
+                    prop = prop['type'];
+                }
+                else{
+                    copyOfNewNode['mandatory'] = false;
+                }
+                // this is the root where we get the value
+                if(prop['kind'] === 'NamedType'){
+                    copyOfNewNode['name'] = prop['name']['value']
+                    newNodeFromMappingTree.forEach(object2 => {
+                        if(object2['name'] === object.name.value){
+                            copyOfNewNode['uri'] = object2['uri'];
+                        }
+                    });
+                }
+                else{
+                    copyOfNewNode['kind'] = prop['kind']
+                    copyOfNewNode['data'] = {}
+                }
+                prop = prop['type'];
+                copyOfNewNode = copyOfNewNode['data'];
+            }
+        });
+
+        newNode['data'] = newNodeData;
+        treeFromSchema[schema.getTypeMap()[schemaTypeName]['name']] = newNode;
+    }
+
+    return treeFromSchema;
+}
+
+
+module.exports = createTree
