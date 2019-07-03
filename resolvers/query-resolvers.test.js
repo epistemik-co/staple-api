fb569d8026b34a93d383013518dc528e624f2374
Artix1500 <artur.haczek23@gmail.com>
2019-07-03 15:32:02 +0200

    Create dumy test sets

---
+++ resolvers/query-resolvers.test.js
@@ -0,0 +1,11 @@
+// const DatabaseInterface = require('../database/database');
+// const database = new DatabaseInterface();
+// const Resolver = require('./resolvers');
+// const rootResolver = new Resolver(database).rootResolver;
+
+describe('My Test Cases for query resolvers', () => {
+    test(`query:`, async () => {
+        return expect('ok').toEqual('ok');
+    })
+
+});
