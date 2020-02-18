e808d98e15bb4da86b587536ad0b4de286242127
Anna Konieczna <ania@skaza.org>
2020-02-18 14:04:36 +0100

    clean up

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function init(ontology, configObject){
     let schema = await schemaFromOntology.generateSchema(ontology);
     schemaObj.schemaSDL =  printSchema(schema);
     schemaObj.schemaMapping = await jsonldFromOntology.process(ontology);
-
+    schemaObj.context = schemaObj.schemaMapping["@context"];
     schemaObj.database = new DatabaseInterface(schemaObj.schemaMapping, configObject);
 
     schemaObj.Warnings = []; // Warnings can be added as object to schemaObj array. Array is clear after each query.
