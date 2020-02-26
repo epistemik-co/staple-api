7ae29cbe148a1f27718a5398b529c70a7fcc30ce
Anna Konieczna <ania@skaza.org>
2020-02-26 09:15:35 +0100

    fix issue#54

--- database/databaseUtilities/backends/mongodb/adapter.js
+++ database/databaseUtilities/backends/mongodb/adapter.js
@@ -39,7 +39,9 @@ class MongodbAdapter {
                     result = await collection.find(query).skip(page * 10 - 10).limit(10).toArray();
                 }
             } else {
-                query["_type"] = _type;
+                if(_type){
+                    query["_type"] = _type;
+                }
                 if (page === undefined) {
                     result = await collection.find(query).toArray();
                 }
