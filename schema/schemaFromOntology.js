8bc6647a4300c163763bbf8caeb486bc21bebe7e
Anna Konieczna <ania@skaza.org>
2020-02-26 09:46:38 +0100

    add _SOURCE to schemaFromOntologyScript

--- schema/schemaFromOntology.js
+++ schema/schemaFromOntology.js
@@ -202,7 +202,8 @@ function createQueryType(classList, filterClassList, classesURIs, propertiesURIs
   var contextType = {
     name: "_CONTEXT", fields: {
       "_id": { type: graphql.GraphQLString, description: "@id" },
-      "_type": { type: graphql.GraphQLString, description: "@type" }
+      "_type": { type: graphql.GraphQLString, description: "@type" },
+      "_SOURCE": {type: graphql.GraphQLEnumType, description: "source description"} //to do
     },
     description : "The mapping from types and properties of the GraphQL schema to the corresponding URIs of the structured data schema."
   };
@@ -238,9 +239,8 @@ function createQueryType(classList, filterClassList, classesURIs, propertiesURIs
       description: String(classList[className].description),
       fields: filterGetFields(filterClassList["Filter" + className])
     });
-    queryType.fields[className] = { type: gqlObjects[className], description: "Get objects of type: " + className, args: { "page": { type: graphql.GraphQLInt }, "inferred": { type: graphql.GraphQLBoolean, defaultValue: false }, "filter": { type: gqlObjects["Filter" + className] } } };
+    queryType.fields[className] = { type: gqlObjects[className], description: "Get objects of type: " + className, args: { "page": { type: graphql.GraphQLInt }, "inferred": { type: graphql.GraphQLBoolean, defaultValue: false }, "filter": { type: gqlObjects["Filter" + className] }, "source": {type: graphql.GraphQLEnumType, defaultValue: 1} } };
   }
-
   queryType = new graphql.GraphQLObjectType(queryType);
   return queryType;
 }
@@ -318,3 +318,45 @@ async function generateSchema(ontology) {
 module.exports = {
   generateSchema: generateSchema
 };
+
+// generateSchema({string: `@prefix schema: <http://schema.org/> .
+// @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+// @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+// @prefix owl: <http://www.w3.org/2002/07/owl#> .
+// @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+// @prefix example: <http://example.com/> .
+// # classes (-> GraphQL types )
+// example:Agent a rdfs:Class ;
+//     rdfs:comment "An agent (individual or legal)" .
+// example:Organization a rdfs:Class ;
+//     rdfs:comment "An organization such as a school, NGO, corporation, club, etc." ;
+//     rdfs:subClassOf example:Agent .
+// example:Person a rdfs:Class ;
+//     rdfs:comment "A person" ;
+//     rdfs:subClassOf example:Agent .
+// # properties ( -> GraphQL fields )
+// example:name a rdf:Property, owl:FunctionalProperty ;
+//     rdfs:comment "Name of the agent" ;
+//     schema:domainIncludes example:Agent ;
+//     schema:rangeIncludes xsd:string .
+// example:age a rdf:Property, owl:FunctionalProperty ;
+//     rdfs:comment "Age of the person" ;
+//     schema:domainIncludes example:Person ;
+//     schema:rangeIncludes xsd:integer .
+// example:isMarried a rdf:Property, owl:FunctionalProperty ;
+//     rdfs:comment "This person is married" ;
+//     schema:domainIncludes example:Person ;
+//     schema:rangeIncludes xsd:boolean .
+// example:revenue a rdf:Property, owl:FunctionalProperty ;
+//     rdfs:comment "The annual revenue of the organization" ;
+//     schema:domainIncludes example:Organization ;
+//     schema:rangeIncludes xsd:decimal .
+// example:employee a rdf:Property ;
+//     rdfs:comment "An employee of an organization" ;
+//     schema:domainIncludes example:Organization ;
+//     schema:rangeIncludes example:Person .
+// example:customerOf a rdf:Property ;
+//     rdfs:comment "An organization this agent is a customer of" ;
+//     schema:domainIncludes example:Agent ;
+//     schema:rangeIncludes example:Organization .`})
+
