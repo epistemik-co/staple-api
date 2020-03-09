5dd1c8a35b4a4b2e458457f9444a73af2c7860e5
Anna Konieczna <ania@skaza.org>
2020-03-09 13:49:25 +0100

    slight changes in demo

--- demo.js
+++ demo.js
@@ -61,7 +61,11 @@ async function Demo() {
                     type: "sparql",
                     url: "http://localhost:3030/staple/sparql", 
                     updateUrl: "http://localhost:3030/staple/update",
+                },
+                memory: {
+                    type: "memory"
                 }
+
             }
         }
     );//, require(appRoot + "/config/database.js"));
