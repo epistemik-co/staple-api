726f854cc15f5d35cb996304e90d0804916843f0
shaqura <43540139+shaqura@users.noreply.github.com>
2020-07-07 20:42:01 +0200

    Update queryTypeResolver.js

--- resolvers/queryResolvers/querys/queryTypeResolver.js
+++ resolvers/queryResolvers/querys/queryTypeResolver.js
@@ -14,7 +14,8 @@ const handleQueryTypeResolver = (database, tree, uri) => {
     return async (parent, args, context, info) => {
 
         logger.info(`Query started for ${uri}`);
-        let data = await database.loadQueryData(info["operation"], uri, args.page /* undefined*/, args.inferred, tree, args.source, args.filter);
+        let limit =args.limit;
+        let data = await database.loadQueryData(info["operation"], uri, args.page /* undefined*/, args.inferred, tree, args.source, args.filter,limit);
         if (args.source != undefined) {
             if (args.source.length > 1) {
                 if (args.page !== undefined) {
